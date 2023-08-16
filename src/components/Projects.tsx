6ee4929d6b1a4788c9cc0075c869883907a3b791
ikram9820 <ikram9820khan@gmail.com>
2023-08-17 00:46:08 +0500

    adding projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,6 +3,8 @@ import project1 from "../assets/images/p1.png";
 import project2 from "../assets/images/p2.png";
 import project3 from "../assets/images/p3.png";
 import project4 from "../assets/images/p4.png";
+import project5 from "../assets/images/p5.png";
+import project6 from "../assets/images/p6.png";
 import "swiper/css";
 import { Pagination, Autoplay } from "swiper/modules";
 import "swiper/css/autoplay";
@@ -32,6 +34,18 @@ const Project = () => {
       img: project4,
       name: "Blog App",
       github_link: "https://github.com/ikram9820/EpicExplora.git",
+      live_link: "www.google.com",
+    },
+    {
+      img: project5,
+      name: "Online Medical Services  App",
+      github_link: "https://github.com/ikram9820/medicalapi.git",
+      live_link: "www.google.com",
+    },
+    {
+      img: project6,
+      name: "Chat app",
+      github_link: "https://github.com/ikram9820/alphaapi.git",
       live_link: "www.google.com",
     },
   ];
