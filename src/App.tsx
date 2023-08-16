d52f4ba1bfb10e6ed47073e3c79ca7cdf98cda6b
ikram9820 <ikram9820khan@gmail.com>
2023-08-16 23:32:03 +0500

    adding navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import Navbar from "./components/Navbar"
 
 function App() {
 
 
   return (
     <>
-     <h1>Hi there</h1>
+     <Navbar/>
     </>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+
+import {BiMenu} from "react-icons/bi";
+
+interface MenuLink  {
+    name:string
+    link:string
+
+}
+const menuLinks:MenuLink[] = [
+   { name:"HOME", link:"#home"},
+   { name:"ABOUT", link:"#about"},
+   { name:"SKILLS", link:"#Skills"},
+   { name:"PROJECTS", link:"#projects"},
+   { name:"CONTACT", link:"#contact"}
+]
+
+const Navbar = () => {
+    const [sticky, setSticky] = useState(false);
+    const [open, setOpen] = useState(false);
+
+    useEffect(()=>{
+        window.addEventListener("scroll", () => {
+            window.scrollY > 0 ? setSticky(true) : setSticky(false);
+        });
+    });
+
+    return(
+        <nav
+        className={`fixed w-full z-[999] justify-between flex  ${
+            sticky ? "bg-white/60 text-gray-900" : "text-white"
+          }`}
+        >
+            <h4 className="text-xl font-bold mx-7 my-3">Ikram</h4>
+
+            <ul
+             className={`${
+                sticky ? "md:bg-white/0" : "bg-white"
+              } text-gray-900 hidden px-10 font-medium rounded-bl-full items-center md:flex gap-16 py-5 text-lg`}      
+            >
+                {menuLinks.map((menu:MenuLink,i:number) =>(
+                    <li key={i} className="hover:text-cyan-600" >
+                        <a href={menu?.link} >{menu?.name}</a>
+                    </li>
+                ))}
+            </ul>
+            
+            <BiMenu
+                onClick = {() =>setOpen(!open)}
+                className ={`z-[999] ${open ? "text-gray-900" : "text-gray-100"} text-3xl md:hidden m-5`}
+            />
+            <ul className={` flex flex-col justify-center gap-10 items-center text-lg md:hidden text-gray-900 absolute w-2/3 h-screen px-7 font-medium bg-white top-0 duration-300 ${open ? "right-0" : "right-[-999px]" }`}>
+                {menuLinks?.map((menu:MenuLink,i:number)=>(
+                    <li key={i}
+                    onClick={() => setOpen(false)}
+                    className="hover:text-cyan-600" >
+                        <a href={menu.link}>{menu.name}</a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
