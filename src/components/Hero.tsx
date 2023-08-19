449e2757af5eb38a5345ebdc38f72257f6d45836
Ikram khan <94529450+ikram9820@users.noreply.github.com>
2023-08-19 14:54:18 +0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,16 @@
 import hero from "../assets/images/hero.png";
 import { FaFacebook,FaLinkedin,FaTwitter,FaGithub} from "react-icons/fa";
-
+import { AiOutlineMail, AiOutlineWhatsApp, AiFillEnvironment } from "react-icons/ai";
+  
+    
 const Hero = () =>{
+
+    const contact_info = [
+      { icon: <AiOutlineMail />, text: "ikram9820khan@gmail.com",link:"mailto:Ikram9820khan@gmail.com" },
+      { icon: <AiOutlineWhatsApp />, text: "+923020185186",link:"https://wa.me/+923020185186" },
+      { icon: <AiFillEnvironment />, text: "Airport Road, Quetta, Pakistan" },
+    ];
+    
     const social_media = [
         {icon: <FaGithub/>,link:"https://github.com/ikram9820/"},
         {icon: <FaLinkedin/>,link:"https://www.linkedin.com/in/ikram-khan-929658249/"},
@@ -26,7 +35,26 @@ const Hero = () =>{
                 <h1 className="text-cyan-600 md:text-6xl text-5xl">Hello!</h1>
                 <h1 className="md:text-5xl text-2xl md:leading-normal leading-10 text-white fond-bold">My Name Is Ikram Khan</h1>
                 <h4 className="md:text-2xl text-lg md:leading-normal leading-5 mt-4 font-bold text-gray-400">FullStack Developer</h4>
-                <a onClick={handleContactClick} className="bg-cyan-600 px-6 py-3 inline-block mt-8 rounded-full cursor-pointer">Contact Me</a>
+
+
+               
+         <ul className="flex flex-col gap-7 list-none">
+            {contact_info?.map((contact, i) => (
+              <li
+                key={i}
+                className="flex flex-row text-left gap-4 flex-wrap items-center"
+              >
+                <div className="min-w-[3.5rem] text-3xl min-h-[3.5rem] flex items-center justify-center text-white bg-cyan-600 rounded-full">
+                   {contact.icon}
+                </div>
+                <div className="md:text-base text-sm break-words">
+                <a href={contact.link}>{contact.text}</a>
+                </div>
+              </li>
+            ))}
+          </ul>
+
+                
                 <ul className="mt-8 text-3xl flex text-gray-600 gap-5">
                     {social_media.map((icon,i:number)=>(
                         <li key={i} className="hover:text-white cursor-pointer">
@@ -34,9 +62,15 @@ const Hero = () =>{
                         </li>
                     ))}
                 </ul>
+                
             </div>
         </section>
     )
 };
 
 export default Hero;
+
+
+
+  
+    
