c29548c9723d8c1fd034d941f061311ad93e3733
Ikram khan <94529450+ikram9820@users.noreply.github.com>
2023-08-19 11:15:11 +0000

    updated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,21 +1,18 @@
 import hero from "../assets/images/hero.png";
 import { FaFacebook,FaLinkedin,FaTwitter,FaGithub} from "react-icons/fa";
-import { AiOutlineMail, AiOutlineWhatsApp, AiFillEnvironment } from "react-icons/ai";
+import { AiOutlineWhatsApp, AiOutlineMail } from "react-icons/ai";
   
     
 const Hero = () =>{
 
-    const contact_info = [
-      { icon: <AiOutlineMail />, text: "ikram9820khan@gmail.com",link:"mailto:Ikram9820khan@gmail.com" },
-      { icon: <AiOutlineWhatsApp />, text: "+923020185186",link:"https://wa.me/+923020185186" },
-      { icon: <AiFillEnvironment />, text: "Airport Road, Quetta, Pakistan" },
-    ];
     
     const social_media = [
         {icon: <FaGithub/>,link:"https://github.com/ikram9820/"},
         {icon: <FaLinkedin/>,link:"https://www.linkedin.com/in/ikram-khan-929658249/"},
         {icon: <FaTwitter/>,link:"https://twitter.com/Ikram9820"},
-        {icon: <FaFacebook/>,link:"https://web.facebook.com/profile.php?id=100094423407781"}
+        {icon: <FaFacebook/>,link:"https://web.facebook.com/profile.php?id=100094423407781"},
+        {icon: <AiOutlineWhatsApp/>, link:"https://wa.me/+923020185186"},
+        {icon: <AiOutlineMail/>, link: "mailto:ikkhan.se@gmail.com"}
     ]
 
 
@@ -31,24 +28,6 @@ const Hero = () =>{
                 <h1 className="md:text-5xl text-2xl md:leading-normal leading-10 text-white fond-bold">My Name Is Ikram Khan</h1>
                 <h4 className="md:text-2xl text-lg md:leading-normal leading-5 mt-4 font-bold text-gray-400">FullStack Developer</h4>
 
-
-               
-         <ul className="flex flex-col mt-8 gap-4 list-none">
-            {contact_info?.map((contact, i) => (
-              <li
-                key={i}
-                className="flex flex-row text-left gap-4 flex-wrap items-center"
-              >
-                <div className="min-w-[2.5rem] text-2xl min-h-[2.5rem] flex items-center justify-center text-white bg-cyan-600 rounded-full">
-                   {contact.icon}
-                </div>
-                <div className="md:text-base text-sm break-words">
-                <a href={contact.link}>{contact.text}</a>
-                </div>
-              </li>
-            ))}
-          </ul>
-
                 
                 <ul className="mt-8 text-3xl flex text-gray-600 gap-5">
                     {social_media.map((icon,i:number)=>(
@@ -59,6 +38,8 @@ const Hero = () =>{
                 </ul>
                 
             </div>
+        
+
         </section>
     )
 };
