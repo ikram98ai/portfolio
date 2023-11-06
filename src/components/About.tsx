b787175cd0e6e0c53272d8d0c7d4b3ee8ce557c5
ikram9820 <ikram9820khan@gmail.com>
2023-11-06 17:52:54 +0500

    adding skill and updating about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,44 +1,48 @@
-
 const About = () => {
-    interface Info {
-        text:string
-        count:string
-    };
-    const info:Info[] = [
-        {text:"Years experience", count:"2"},
-        {text:"Projects completed", count:"6"},
-        {text:"Certificates", count:"2"},
-    ];
+  interface Info {
+    text: string;
+    count: string;
+  }
+  const info: Info[] = [
+    { text: "Years experience", count: "2" },
+    { text: "Projects completed", count: "6" },
+    { text: "Certificates", count: "2" },
+  ];
 
-    return (
-        <section id="about" className="pb-10 text-white">
-            <div className="text-4xl font-semibold  flex justify-center gap-2">
-                <h3>About</h3>
-                <h3 className="text-cyan-600">Me</h3>
-            </div>
-            <p className="text-gray-400 text-center font-semibold">My Introduction</p>
-            <div className="flex flex-col items-center md:gap-6 gap-12 max-w-6xl mx-auto text-gray-300 mt-5">
-                <p className="text-justify leading-7 w-11/12 mx-auto">
-                As a skilled Software Engineer, I possess a strong understanding of both web development and software engineering
-                principles. I have a deep knowledge of Python and Django, which allows me to write highly maintainable, clean, and 
-                reusable code, and efficiently building robust and scalable Software. Moreover, I really like learning new things, 
-                especially about technology, so I can stay up to date with the fast-changing tech world.
-                </p>
-                <ul className="mt-10 mx-4 flex items-center justify-center md:gap-48 gap-6">
-                    {info.map((content,i)=>(
-                        <li key={i}>
-                            <div className="flex-col">
-                                <div className="flex">
-                                    <h3 className="md:text-4xl text-2xl font-semibold text-white"> {content.count} </h3>
-                                    <h3 className="text-cyan-600 font-bold text-2xl">+</h3>
-                                </div>
-                                <h3 className="md:text-base text-xs">{content.text}</h3>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </section>
-    );
+  return (
+    <section id="about" className="pb-10 text-white">
+      <div className="text-4xl font-semibold  flex justify-center gap-2">
+        <h3>About</h3>
+        <h3 className="text-cyan-600">Me</h3>
+      </div>
+      <p className="text-gray-400 text-center font-semibold">My Introduction</p>
+      <div className="flex flex-col items-center md:gap-6 gap-12 max-w-6xl mx-auto text-gray-300 mt-5">
+        <p className="text-justify leading-7 w-11/12 mx-auto">
+          Highly skilled and motivated Full-stack developer with hands-on
+          experience in developing scalable web applications. Committed to
+          delivering efficient and robust code solutions, I strive to contribute
+          effectively to a dynamic team environment. Seeking a challenging role
+          to leverage my expertise and further enhance my skills in a
+          growth-oriented organization.
+        </p>
+        <ul className="mt-10 mx-4 flex items-center justify-center md:gap-48 gap-6">
+          {info.map((content, i) => (
+            <li key={i}>
+              <div className="flex-col">
+                <div className="flex">
+                  <h3 className="md:text-4xl text-2xl font-semibold text-white">
+                    {" "}
+                    {content.count}{" "}
+                  </h3>
+                  <h3 className="text-cyan-600 font-bold text-2xl">+</h3>
+                </div>
+                <h3 className="md:text-base text-xs">{content.text}</h3>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  );
 };
 export default About;
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,17 +1,45 @@
-import { FaDocker, FaPython } from "react-icons/fa";
-import { DiPostgresql} from "react-icons/di";
-import { SiPytorch, SiDjango} from "react-icons/si";
-import { BsGit} from "react-icons/bs";
-
+import { FaPython } from "react-icons/fa";
+import { DiPostgresql } from "react-icons/di";
+import { SiPytorch, SiDjango, SiReact, SiTypescript } from "react-icons/si";
 
 const Skills = () => {
   const skills = [
-    { icon: <FaPython />,name:"Python", skill:"Programming", level: "Expert" },
-    { icon: <SiDjango />,name:"Django", skill:"Web Development",level: "Expert" },
-    { icon: <DiPostgresql />,name:"PostgreSQL", skill:"Database System",level: "Intermediate" },
-    { icon: <SiPytorch />,name:"Pytorch", skill:"Machine Learning",level: "Intermediate" },
-    { icon: <BsGit />,name:"Git", skill:"Version Control",level: "Beginner" },
-    { icon: <FaDocker />,name:"Docker", skill:"Containerization",level: "Beginner" },
+    {
+      icon: <FaPython />,
+      name: "Python",
+      skill: "Programming",
+      level: "Expert",
+    },
+    {
+      icon: <SiDjango />,
+      name: "Django",
+      skill: "Back-end Development",
+      level: "Expert",
+    },
+    {
+      icon: <SiTypescript />,
+      name: "Typescript",
+      skill: "Programming",
+      level: "Expert",
+    },
+    {
+      icon: <SiReact />,
+      name: "React",
+      skill: "Front-end Development",
+      level: "Expert",
+    },
+    {
+      icon: <DiPostgresql />,
+      name: "PostgreSQL",
+      skill: "Database System",
+      level: "Intermediate",
+    },
+    {
+      icon: <SiPytorch />,
+      name: "Pytorch",
+      skill: "Machine Learning",
+      level: "Intermediate",
+    },
   ];
   return (
     <section
@@ -27,8 +55,12 @@ const Skills = () => {
           <li key={i}>
             <div className="text-5xl flex justify-center">{skill.icon}</div>
             <div className="text-sm font-bold">{skill.name}</div>
-            <div className="text-xxs mt-2 font-bold text-gray-500">{skill.skill}</div>
-            <div className="text-xxs font-bold text-gray-500">{skill.level}</div>
+            <div className="text-xxs mt-2 font-bold text-gray-500">
+              {skill.skill}
+            </div>
+            <div className="text-xxs font-bold text-gray-500">
+              {skill.level}
+            </div>
           </li>
         ))}
       </ul>
